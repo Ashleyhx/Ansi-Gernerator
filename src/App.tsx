97e754f50b0e6cc6c2593af0d2da739e244d383f
Ashley Huo <x2huo@uwaterloo.ca>
2024-05-11 20:24:03 -0400

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 import '@mantine/core/styles.css';
 import Ansi from "ansi-to-react";
@@ -11,6 +11,17 @@ import {
     Stack,
     Tooltip
 } from '@mantine/core';
+import {Flex, FlexProps, Layout} from "antd";
+import {Header, Content} from "antd/lib/layout/layout";
+
+const boxStyle: React.CSSProperties = {
+    width: '100%',
+    height: 320,
+    borderRadius: 6,
+    border: '1px solid #40a9ff',
+};
+
+
 
 function App() {
   const [ansiCodeText, setAnsiCodeText] = useState('');
@@ -29,48 +40,56 @@ function App() {
     useEffect(() => {
         const cleanColor = background.replace('rgba(', '').replace(')', '');
         const [r, g, b] = cleanColor.split(',').map((value) => parseInt(value, 10) || 0);
-        setAnsiCodeBackground(`\x1b[48;2;${r};${g};${b}m\x1b[4m`);
+        setAnsiCodeBackground(`\x1b[48;2;${r};${g};${b}m`);
         setTextBackground(`\\x1b[48;2;${r};${g};${b}m`);
     }, [background]);
 
-
   return (
-      <>
-          <h1>Ansi Code Generator</h1>
-          <p>RGB ANSI Code is: {textCode + textBackground}</p>
+      <Layout>
+          <Header color='white'>Ansi Code Generator</Header>
+          <Content>
+              <p>RGB ANSI Code is: {textCode + textBackground}</p>
 
-          <CopyButton value={textCode + textBackground}>
-              {({ copied, copy }) => (
-                  <Button
-                      color={(copied ? 'teal' : 'blue') as MantineColor}
-                      onClick={copy}
-                  >
-                      {copied ? 'Copied' : 'Copy'}
-                  </Button>
-              )}
-          </CopyButton>
+              <CopyButton value={textCode + textBackground}>
+                  {({copied, copy}) => (
+                      <Button
+                          color={(copied ? 'teal' : 'blue') as MantineColor}
+                          onClick={copy}
+                      >
+                          {copied ? 'Copied' : 'Copy'}
+                      </Button>
+                  )}
+              </CopyButton>
 
-          <CopyButton value="https://mantine.dev" timeout={2000}>
-              {({ copied, copy }) => (
-                  <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position="right">
-                      <ActionIcon color={(copied ? 'teal' : 'blue') as MantineColor} variant="subtle" onClick={copy}>
-                      </ActionIcon>
-                  </Tooltip>
-              )}
-          </CopyButton>
-          <h2><Ansi>{`${ansiCodeText}${ansiCodeBackground} Test`}</Ansi></h2>
+              <CopyButton value="https://mantine.dev" timeout={2000}>
+                  {({copied, copy}) => (
+                      <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position="right">
+                          <ActionIcon color={(copied ? 'teal' : 'blue') as MantineColor} variant="subtle"
+                                      onClick={copy}>
+                          </ActionIcon>
+                      </Tooltip>
+                  )}
+              </CopyButton>
+              <h2><Ansi>{`${ansiCodeText}${ansiCodeBackground} Test`}</Ansi></h2>
+
+              <Flex style={boxStyle} justify='center' align='center' gap='large'>
+                  <Stack>
+                      <h3> Text color </h3>
+                      <ColorPicker format="rgba" value={color} onChange={setColor}/>
+                  </Stack>
+
+                  <Stack>
+                      <h3> Background color </h3>
+                      <ColorPicker format="rgba" value={background} onChange={setBackground}/>
+                  </Stack>
+              </Flex>
 
 
-          <h3> Text color </h3>
-          <Stack>
-              <ColorPicker format="rgba" value={color} onChange={setColor} />
-          </Stack>
+          </Content>
 
-          <h3> Background color </h3>
-          <Stack>
-              <ColorPicker format="rgba" value={background} onChange={setBackground} />
-          </Stack>
-      </>
+
+      </Layout>
+
   )
 }
 
